8a5efd5e4af685f7c5a39f96ef6cab70c5dbbc66
sophia daniels <sophiathekitty@users.noreply.github.com>
2018-04-30 19:52:44 -0600

    added icons for scriptable objects

---
+++ Scripts/Editor/CopyIconsToGizmos.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class CopyIconsToGizmos {
+    static CopyIconsToGizmos()
+    {
+        FileUtil.CopyFileOrDirectory("Assets/SharedVariableSaveSystem/Scripts/Editor/Icons/", "Assets/Gizmos/");
+    }
+}
---
+++ Scripts/Editor/SaveObjectEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SaveObject))]
+public class SaveObjectEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if(GUILayout.Button("Clear Save Data"))
+        {
+            SaveObject so = (SaveObject)target;
+            so.clearSaveData();
+        }
+    }
+    public override Texture2D RenderStaticPreview(string assetPath, Object[] subAssets, int width, int height)
+    {
+        return base.RenderStaticPreview(assetPath, subAssets, width, height);
+    }
+}
