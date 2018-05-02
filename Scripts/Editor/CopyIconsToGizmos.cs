b8c0440b62f24e7c3f7294931096384f299567c3
sophia daniels <sophiathekitty@users.noreply.github.com>
2018-05-02 02:42:12 -0600

    better copy icons to gizmos
    it was throwing errors so i made it better. now it doesn't throw errors after it's copied the files.

--- Scripts/Editor/CopyIconsToGizmos.cs
+++ Scripts/Editor/CopyIconsToGizmos.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using System.IO;
 
 [InitializeOnLoad]
 public class CopyIconsToGizmos {
     static CopyIconsToGizmos()
     {
-        FileUtil.CopyFileOrDirectory("Assets/SharedVariableSaveSystem/Scripts/Editor/Icons/", "Assets/Gizmos/");
+        if (!File.Exists("Assets/Gizmos/SaveObject Icon.png"))
+        {
+            Debug.Log("Copy SharedVariableSaveSystem Icons");
+            if (!Directory.Exists("Assets/Gizmos"))
+                Directory.CreateDirectory("Assets/Gizmos");
+            File.Copy("Assets/SharedVariableSaveSystem/Scripts/Editor/Icons/SaveObject Icon.png", "Assets/Gizmos/SaveObject Icon.png");
+            File.Copy("Assets/SharedVariableSaveSystem/Scripts/Editor/Icons/BoolVariable Icon.png", "Assets/Gizmos/BoolVariable Icon.png");
+            File.Copy("Assets/SharedVariableSaveSystem/Scripts/Editor/Icons/IntVariable Icon.png", "Assets/Gizmos/IntVariable Icon.png");
+            File.Copy("Assets/SharedVariableSaveSystem/Scripts/Editor/Icons/FloatVariable Icon.png", "Assets/Gizmos/FloatVariable Icon.png");
+            File.Copy("Assets/SharedVariableSaveSystem/Scripts/Editor/Icons/StringVariable Icon.png", "Assets/Gizmos/StringVariable Icon.png");
+            File.Copy("Assets/SharedVariableSaveSystem/Scripts/Editor/Icons/Vector3Variable Icon.png", "Assets/Gizmos/Vector3Variable Icon.png");
+            File.Copy("Assets/SharedVariableSaveSystem/Scripts/Editor/Icons/Vector3IntVariable Icon.png", "Assets/Gizmos/Vector3IntVariable Icon.png");
+        }
     }
 }
