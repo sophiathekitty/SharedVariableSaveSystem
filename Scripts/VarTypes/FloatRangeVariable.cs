529ed46d998e2d171d05b6286cec0cc5d2a4332d
sophia daniels <sophiathekitty@users.noreply.github.com>
2018-09-06 15:27:29 -0600

    float and int range variables

---
+++ Scripts/VarTypes/FloatRangeVariable.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Variables/Float Range Variable")]
+public class FloatRangeVariable : SavableVariable, ISerializationCallbackReceiver
+{
+    public float MinValue;
+    public float MaxValue;
+    public bool Descending;
+
+    [System.NonSerialized]
+    private float _RuntimeValue;
+
+    public float RuntimeValue
+    {
+        get
+        {
+            return _RuntimeValue;
+        }
+        set
+        {
+            _RuntimeValue = value;
+            FitToRange();
+        }
+    }
+    public float Percent
+    {
+        get
+        {
+            if (Descending)
+                return (1 - ((_RuntimeValue - MinValue) / (MaxValue - MinValue)));
+            return ((_RuntimeValue - MinValue) / (MaxValue - MinValue));
+        }
+        set
+        {
+            if (Descending)
+                _RuntimeValue = Mathf.Lerp(MaxValue, MinValue, value);
+            else
+                _RuntimeValue = Mathf.Lerp(MinValue, MaxValue, value);
+            FitToRange();
+        }
+    }
+    private void FitToRange()
+    {
+        if (_RuntimeValue > MaxValue)
+            _RuntimeValue = MaxValue;
+        if (_RuntimeValue < MinValue)
+            _RuntimeValue = MinValue;
+    }
+
+    public void OnAfterDeserialize()
+    {
+        RuntimeValue = MinValue;
+    }
+
+    public void OnBeforeSerialize() { /* do nothing */ }
+
+    public override string OnSaveData()
+    {
+        return RuntimeValue.ToString();
+    }
+
+    public override void OnLoadData(string data)
+    {
+        RuntimeValue = float.Parse(data);
+        loaded = true;
+    }
+    public override void OnClearSave()
+    {
+        OnAfterDeserialize();
+    }
+
+}
---
+++ Scripts/VarTypes/IntRangeVariable.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Variables/Int Range Variable")]
+public class IntRangeVariable : SavableVariable, ISerializationCallbackReceiver
+{
+    public int MinValue;
+    public int MaxValue;
+    public bool Descending;
+
+    [System.NonSerialized]
+    private int _RuntimeValue;
+
+    public int RuntimeValue
+    {
+        get
+        {
+            return _RuntimeValue;
+        }
+        set
+        {
+            _RuntimeValue = value;
+            FitToRange();
+        }
+    }
+    public float Percent
+    {
+        get
+        {
+            if (Descending)
+                return (1 - (((float)_RuntimeValue - (float)MinValue) / ((float)MaxValue - (float)MinValue)));
+            return (((float)_RuntimeValue - (float)MinValue) / ((float)MaxValue - (float)MinValue));
+        }
+        set
+        {
+            if (Descending)
+                _RuntimeValue = Mathf.RoundToInt(Mathf.Lerp((float)MaxValue, (float)MinValue, (float)value));
+            else
+                _RuntimeValue = Mathf.RoundToInt(Mathf.Lerp((float)MinValue, (float)MaxValue, (float)value));
+            FitToRange();
+        }
+    }
+    private void FitToRange()
+    {
+        if (_RuntimeValue > MaxValue)
+            _RuntimeValue = MaxValue;
+        if (_RuntimeValue < MinValue)
+            _RuntimeValue = MinValue;
+    }
+
+    public void OnAfterDeserialize()
+    {
+        RuntimeValue = MinValue;
+    }
+
+    public void OnBeforeSerialize() { /* do nothing */ }
+
+    public override string OnSaveData()
+    {
+        return RuntimeValue.ToString();
+    }
+
+    public override void OnLoadData(string data)
+    {
+        RuntimeValue = int.Parse(data);
+        loaded = true;
+    }
+    public override void OnClearSave()
+    {
+        OnAfterDeserialize();
+    }
+}
